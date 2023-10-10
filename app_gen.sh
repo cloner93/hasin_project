f216318b0f2f342b4b6b0a09ae8409a259574ae9
cloner93 <targholi2112@gmail.com>
2023-10-10 11:41:53 +0330

    edit app_gen.sh for create apk and rename it and save in apk folder.

--- app_gen.sh
+++ app_gen.sh
@@ -1,14 +1,28 @@
 #!/usr/bin/env bash
 
- DIR="$( pwd )"
- APP_OUT=$DIR/app/build/outputs
+# Get the current directory
+DIR="$(pwd)"
+# Set the output directory for the app
+APP_OUT="$DIR/app/build/outputs"
 
- # Build the debug variant
- GRADLE_PARAMS=" --stacktrace"
- $DIR/gradlew :app:clean :app:assembleDebug ${GRADLE_PARAMS}
- BUILD_RESULT=$?
+# APK and log file names based on the APK
+DEBUG_APK_NAME="app_hasin_project_debug_$(git symbolic-ref --short HEAD)_$(date +%Y_%m_%d_%H_%M).apk"
+LOG_FILE="$DIR/APK/log/build_log_$(git symbolic-ref --short HEAD)_$(date +%Y_%m_%d_%H_%M).txt"
 
- # debug apk
- cp $APP_OUT/apk/debug/app-debug.apk $DIR/"apk_debug_$(date +%Y_%m_%d_%H_%M).apk"
+# Build the debug variant and log the output
+GRADLE_PARAMS="--stacktrace"
 
- exit $BUILD_RESULT
+# Create the log directory if it doesn't exist
+mkdir -p "$DIR/APK/log"
+
+"$DIR/gradlew" :app:clean :app:assembleDebug $GRADLE_PARAMS > "$LOG_FILE" 2>&1
+BUILD_RESULT=$?
+
+# Copy the debug APK to APK/debug folder if build is successful
+if [ $BUILD_RESULT -eq 0 ]; then
+    mkdir -p "$DIR/APK/debug"
+    cp "$APP_OUT/apk/debug/app-debug.apk" "$DIR/APK/debug/$DEBUG_APK_NAME"
+fi
+
+# Exit with the build result
+exit $BUILD_RESULT
