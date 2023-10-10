2b1e70aa50316676df4447fc3e7fe833ad898772
cloner93 <targholi2112@gmail.com>
2023-10-10 12:58:03 +0330

    edit app_gen.sh file.

--- app_gen.sh
+++ app_gen.sh
@@ -5,9 +5,12 @@ DIR="$(pwd)"
 # Set the output directory for the app
 APP_OUT="$DIR/app/build/outputs"
 
+# app name contain git branch and current date and time
+APP_NAME="$(git symbolic-ref --short HEAD)_$(date +%Y_%m_%d_%H_%M)"
+
 # APK and log file names based on the APK
-DEBUG_APK_NAME="app_hasin_project_debug_$(git symbolic-ref --short HEAD)_$(date +%Y_%m_%d_%H_%M).apk"
-LOG_FILE="$DIR/APK/log/build_log_$(git symbolic-ref --short HEAD)_$(date +%Y_%m_%d_%H_%M).txt"
+DEBUG_APK_NAME="app_hasin_project_debug_$APP_NAME.apk"
+LOGS_FILE_NAME="build_log_$APP_NAME.txt"
 
 # Build the debug variant and log the output
 GRADLE_PARAMS="--stacktrace"
@@ -15,7 +18,7 @@ GRADLE_PARAMS="--stacktrace"
 # Create the log directory if it doesn't exist
 mkdir -p "$DIR/APK/log"
 
-"$DIR/gradlew" :app:clean :app:assembleDebug $GRADLE_PARAMS > "$LOG_FILE" 2>&1
+"$DIR/gradlew" :app:clean :app:assembleDebug ${GRADLE_PARAMS} > "$DIR/APK/log/$LOGS_FILE_NAME" 2>&1
 BUILD_RESULT=$?
 
 # Copy the debug APK to APK/debug folder if build is successful
